4aaee27a54c5d932881c06a9ef5becfad237286b
kcy230426 <132032089+kcy230426@users.noreply.github.com>
2023-06-20 11:27:54 +0900

    second commit
    991 이하일때 앞뒤버튼 제거 수정

--- js/main.js
+++ js/main.js
@@ -5,4 +5,17 @@ var swiper = new Swiper("#section .swiper", {
         delay: 2500,
         disableOnInteraction: false,
       },
+
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    
+      scrollbar: {
+        el: '.swiper-scrollbar',
+      },
   });
